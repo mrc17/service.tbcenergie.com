96ce3c12db4b7d0b824e8301f0e7b8c6aff1aded
Axel <axeltoure1eme@gmail.com>
2023-07-23 18:10:47 +0000

    Function pour afficher la vue

--- app/Http/Controllers/siteController.php
+++ app/Http/Controllers/siteController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class siteController extends Controller
 {
-    //
+    //Affichage de la page de site
+
+    public function index(){
+        return view('Auth.site');
+    }
 }
