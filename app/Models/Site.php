15fce164725f9ada00cfa26f34ca086a5ce25a24
Axel <axeltoure1eme@gmail.com>
2023-07-24 00:02:12 +0000

    Ajout de la relation ville et Topologie

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Ville;
+use App\Models\Topologie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Site extends Model
 {
     use HasFactory;
+
+    public function ville()
+    {
+        return $this->belongsTo(Ville::class, 'clustomer_id',);
+    }
+
+    public function Topologie(){
+        return $this->belongsTo(Topologie::class,'topologie_id');
+    }
+
 }
