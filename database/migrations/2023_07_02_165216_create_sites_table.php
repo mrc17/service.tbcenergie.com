ee31dd2e4480b4ed9c3d28f02869ef578cef8d0b
Axel <axeltoure1eme@gmail.com>
2023-07-24 00:03:41 +0000

    Ajout de la colonne matricule

--- database/migrations/2023_07_02_165216_create_sites_table.php
+++ database/migrations/2023_07_02_165216_create_sites_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('nom');
             $table->foreignId('clustomer_id')->constrained('villes');
             $table->foreignId('topologie_id')->constrained('topologies');
+            $table->string('matricule');
             $table->timestamps();
         });
     }
