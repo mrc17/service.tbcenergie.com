0b497b3d0d67795ae01eac8a31f2dde3afb5af28
Axel <axeltoure1eme@gmail.com>
2023-07-24 00:01:47 +0000

    Ajout de la relation sites

--- app/Models/Topologie.php
+++ app/Models/Topologie.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Topologie extends Model
 {
     use HasFactory;
+
+
+    public function sites(){
+        return $this->hasMany(site::class,'topologie_id');
+    }
+
 }
