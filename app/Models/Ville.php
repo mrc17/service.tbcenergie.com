f2ee50e32ffa3ce3d9e83bafacbdd792427a2a71
Axel <axeltoure1eme@gmail.com>
2023-07-24 00:02:30 +0000

    Ajout de la relation sites

--- app/Models/Ville.php
+++ app/Models/Ville.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Ville extends Model
 {
     use HasFactory;
+
+    public function sites()
+    {
+        return $this->hasMany(Site::class, 'clustomer_id');
+    }
 }
