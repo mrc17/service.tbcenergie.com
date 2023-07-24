1d5c4ee5ec4fdeda1ca08f6b1a752f3e4d56324a
Axel <axeltoure1eme@gmail.com>
2023-07-24 07:11:34 +0000

    Ajout de la recherche d'un site

--- app/Http/Controllers/siteController.php
+++ app/Http/Controllers/siteController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Site;
-use Illuminate\Http\Request;
+use App\Http\Requests\SearchRequest;
 
 class SiteController extends Controller
 {
@@ -12,12 +12,21 @@ class SiteController extends Controller
     {
         // Récuperation des informations des sites
         $allSites = Site::all();
-        $count=1;
-        return view('Auth.site', ['allSites' => $allSites,'count'=>$count]);
+        $count = 1;
+        return view('Auth.site', ['allSites' => $allSites, 'count' => $count]);
     }
 
+    public function search(SearchRequest $request)
+    {
+        $sites = Site::where('matricule', $request->input('search'))
+            ->orWhere('nom', $request->input('search'))
+            ->get();
 
-    public function search(){
-
+        if ($sites) {
+            $count = 1;
+            return view('Auth.site', ['allSites' => $sites, 'count' => $count]);
+        } else {
+            return redirect('site')->with('error', 'Aucun site n\'a été trouvé');
+        }
     }
 }
