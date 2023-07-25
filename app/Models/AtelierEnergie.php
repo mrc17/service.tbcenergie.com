914dbee5bcd2e49d0bacb07876e28c83b4af0797
Axel <axeltoure1eme@gmail.com>
2023-07-25 16:39:19 +0000

    relation pour recuperer la baie associée au  Atelier d'energie

--- app/Models/AtelierEnergie.php
+++ app/Models/AtelierEnergie.php
@@ -1,16 +1,26 @@
 <?php
 
 namespace App\Models;
+
+use App\Models\Baie;
 use App\Models\Site;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class AtelierEnergie extends Model
 {
     use HasFactory;
 
     //relation pour recuperer le sites associée au AtelierEnergie
 
-    public function site(){
-        return $this->belongsTo(Site::class,'atl_ener_id','id');
+    public function site()
+    {
+        return $this->belongsTo(Site::class, 'atl_ener_id', 'id');
+    }
+
+    //relation pour recuperer la baie associée au  Atelier d'energie
+    public function baies()
+    {
+        return $this->belongsTo(Baie::class,"baie_id",'id');
     }
 }
