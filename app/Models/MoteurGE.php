a6b5f555bb7c8d37bacf5741c3977b561428badd
Axel <axeltoure1eme@gmail.com>
2023-07-25 15:07:54 +0000

    Relation pour recuperer le generateur associer au moteur

--- app/Models/MoteurGE.php
+++ app/Models/MoteurGE.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MoteurGE extends Model
 {
     use HasFactory;
+
+    //Relation pour recuperer le generateur associer au moteur
+
+    public function generateurs(){
+        return $this->hasMany(MoteurGE::class,'moteur_id');
+    }
+
 }
