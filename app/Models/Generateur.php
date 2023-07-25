b7dd0d2a644dd84475b53d8fd167097f9024a979
Axel <axeltoure1eme@gmail.com>
2023-07-25 10:52:52 +0000

    Ajout de la relation marqueGE et site

--- app/Models/Generateur.php
+++ app/Models/Generateur.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Site;
+use App\Models\MarqueGE;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Generateur extends Model
 {
     use HasFactory;
+
+    public function site()
+    {
+        return $this->belongsTo(Site::class, 'clustomer_id'); // Correction de la clé étrangère
+    }
+
+
+    public function marqueGE()
+    {
+        return $this->belongsTo(MarqueGE::class,'');
+    }
 }
