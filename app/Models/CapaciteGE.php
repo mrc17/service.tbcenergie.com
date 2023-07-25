504d271b9004c8186c0601e3726d051951e8e53b
Axel <axeltoure1eme@gmail.com>
2023-07-25 14:54:59 +0000

    Relation inverse pour récupérer les generateurs associés à la marque

--- app/Models/CapaciteGE.php
+++ app/Models/CapaciteGE.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Generateur;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CapaciteGE extends Model
 {
     use HasFactory;
+
+    // Relation inverse pour récupérer les generateurs associés à la marque
+    public function generateurs()
+    {
+        return $this->hasMany(Generateur::class, "marque_id");
+    }
 }
+
