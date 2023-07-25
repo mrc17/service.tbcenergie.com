4419e9bbde8a00a9dd614d434b6919eea9291425
Axel <axeltoure1eme@gmail.com>
2023-07-25 16:39:35 +0000

    relation pour recuperer la Atelier d'energie associée au baies

--- app/Models/Baie.php
+++ app/Models/Baie.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\AtelierEnergie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Baie extends Model
 {
     use HasFactory;
+
+        //relation pour recuperer la Atelier d'energie associée au baies
+
+        public function AtelierEnergies(){
+            return $this->hasMany(AtelierEnergie::class,"baie_id");
+        }
 }
