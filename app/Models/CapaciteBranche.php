36b30f3568c5a72eb76ec784577cb966ddb7d234
Axel <axeltoure1eme@gmail.com>
2023-07-26 07:17:14 +0000

    relation pour recuperer la batterie  associée au capacite_branches

--- app/Models/CapaciteBranche.php
+++ app/Models/CapaciteBranche.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class CapaciteBranche extends Model
 {
     use HasFactory;
+    //relation pour recuperer la batterie  associée au capacite_branches
+    public function batteries(){
+        return $this->hasMany(Batterie::class,'capacite_id','id');
+    }
 }
