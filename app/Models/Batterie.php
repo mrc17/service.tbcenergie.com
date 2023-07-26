9de1f63fcf93de6ad54205f29d1249ff3905f6d1
Axel <axeltoure1eme@gmail.com>
2023-07-26 07:14:31 +0000

    relation pour recuperer le site associé à la batterie

--- app/Models/Batterie.php
+++ app/Models/Batterie.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Site;
+use App\Models\MarqueBat;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Batterie extends Model
 {
     use HasFactory;
+
+    //relation pour recuperer le site associé à la batterie
+    public function site()
+    {
+        return $this->belongsTo(Site::class, 'batterie_id');
+    }
+
 }
