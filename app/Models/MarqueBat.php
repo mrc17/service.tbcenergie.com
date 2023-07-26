41e6d6cefac40e6ba4bd5855346dc1434f8c1994
Axel <axeltoure1eme@gmail.com>
2023-07-26 07:17:26 +0000

    relation pour recuperer les batteries associées à la marque

--- app/Models/MarqueBat.php
+++ app/Models/MarqueBat.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Batterie;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MarqueBat extends Model
 {
     use HasFactory;
+
+    //relation pour recuperer les batteries associées à la marque
+    public function batteries()
+    {
+        return $this->hasMany(Batterie::class, 'bat_id', 'id');
+    }
 }
